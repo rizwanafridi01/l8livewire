1298143aea37767f7f6caf9081a0eda543a18096
Rizwan Afridi <rizwanafridi@Rizwan-afridi.local>
2020-10-05 23:31:47 +0400

    Laravel livewire part 1

---
+++ app/Http/Livewire/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HelloWorld extends Component
+{
+    public $name = "Rizwan";
+    public function render()
+    {
+        return view('livewire.hello-world');
+    }
+}
---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    <h3>Because she competes with no one, {{ $name }}</h3>
+</div>

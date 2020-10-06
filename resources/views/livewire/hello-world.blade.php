f92f87ff2cdcbc05c9f5350fc29aa478a1366d97
Rizwan Afridi <rizwanafridi@Rizwan-afridi.local>
2020-10-06 21:23:22 +0400

    debounce and lazy bound part 3 fixed

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
     <h3>Because she competes with no one, {{ $name }}</h3>
-    <p><input type="text" wire:model.rebounce.1000ms="message1"></p>
+    <p><input type="text" wire:model.debounce.1000ms="message1"></p>
     <p><input type="text" wire:model.lazy="message2"></p>
 
     {{ $message1 }}
